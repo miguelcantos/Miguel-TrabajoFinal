6a736cd74e9414903edee96f83f468ce75ffe24a
DAW <miguelcantos69@gmail.com>
2019-05-13 20:17:18 +0200

    miguel Funcionamiento de usuario y contraseña (pagina principal)

---
+++ src/proyectofinal/Empleados.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinal;
+
+import DAO.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author DAW
+ */
+public class Empleados {
+    
+    private String idEmpleado;
+    private String nombreUsuario;
+    private String contrasenya;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String ciudad;
+    private String telefono;
+    private String email;
+    private double salario;
+    private ArrayList<Empleados> array = new ArrayList<Empleados>();
+    
+    public String getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(String idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenya() {
+        return contrasenya;
+    }
+
+    public void setContrasenya(String contrasenya) {
+        this.contrasenya = contrasenya;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public Empleados(String idEmpleado, String nombreUsuario, String contrasenya, String nombre, String apellido, String direccion, String ciudad, String telefono, String email, double salario) {
+        this.idEmpleado = idEmpleado;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenya = contrasenya;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+        this.telefono = telefono;
+        this.email = email;
+        this.salario = salario;
+    }
+    
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////  
+    
+    
+
+}
--- src/proyectofinal/FXMLDocumentController.java
+++ src/proyectofinal/FXMLDocumentController.java
@@ -5,15 +5,26 @@
  */
 package proyectofinal;
 
+import DAO.Conexion;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -25,45 +36,132 @@ import javafx.stage.StageStyle;
  */
 public class FXMLDocumentController implements Initializable {
 
+    
+    
+    @FXML
+    private TextField usuario;
+    @FXML
+    private PasswordField contrasenya;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    
     }    
 
     @FXML
     private void abrirInvitado(ActionEvent event) {
-         try {
-            FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("pantallaVista.fxml"));
-            Parent root1= (Parent)fxmlLoader.load();
-            Stage stage= new Stage();
-            stage.setScene(new Scene(root1));
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.show();
+        
+            
+             try {
+                FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("pantallaVista.fxml"));
+                Parent root1= (Parent)fxmlLoader.load();
+                Stage stage= new Stage();
+                stage.setScene(new Scene(root1));
+                stage.initModality(Modality.APPLICATION_MODAL);
+                stage.initStyle(StageStyle.UNDECORATED);
+                stage.show();
     
-        } catch (IOException e) {
-        }
+             } catch (IOException e) {
+             }
+            
+           
     }
 
     @FXML
     private void abrirIniciarSesion(ActionEvent event) {
         
+        ObservableList<Empleados> lista= FXCollections.observableArrayList();
         
+        PreparedStatement stmt= null;
+        ResultSet rs = null;
+        boolean salir=true;
         
-        try {
-            FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("pantallaModificacion.fxml"));
-            Parent root1= (Parent)fxmlLoader.load();
-            Stage stage= new Stage();
-            stage.setScene(new Scene(root1));
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.show();
-    
-        } catch (IOException e) {
+        Conexion conexion = new Conexion();
+        Connection con = conexion.conectar();
+        
+            try {
+                
+                stmt = con.prepareStatement("SELECT * From empleado");
+                rs = stmt.executeQuery();   
+            
+                while(rs.next()){ 
+                    Empleados E = new Empleados (rs.getString("idEmpleado"), rs.getString("nombreUsuario"), rs.getString("contrasenya"), rs.getString("nombre"), rs.getString("apellido"), rs.getString("direccion"), rs.getString("ciudad"), rs.getString("telefono"), rs.getString("email"), rs.getDouble("salario"));
+                    lista.add(E);
+            }    
+            } catch (SQLException e) {
+            }finally{
+                try {
+                    if (rs != null) {
+                        rs.close();
+                    }
+                    if (stmt != null) {
+                        stmt.close();
+                    }    
+                } catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }finally{            
+                    conexion.desconectar(con);
+                }
+            }
+        
+                  
+            for (int i = 0; i < lista.size(); i++) {
+            
+                if(usuario.getText().equals(lista.get(i).getNombreUsuario()) && contrasenya.getText().equals(lista.get(i).getContrasenya())){
+                
+                    try {
+                        FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("pantallaModificacion.fxml"));
+                        Parent root1= (Parent)fxmlLoader.load();
+                        Stage stage= new Stage();
+                        stage.setScene(new Scene(root1));
+                        stage.initModality(Modality.APPLICATION_MODAL);
+                        stage.initStyle(StageStyle.UNDECORATED);
+                        stage.show();
+                        
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    i=100;
+                    salir=false;
+                }    
+            }
+            
+            
+        if(salir==true){
+        
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("ERROR");
+            alert.setHeaderText(null);
+            alert.setContentText(" ERROR DE USUARIO O CONTRASEÑA ");
+            alert.showAndWait();
+            
+            usuario.setText("");
+            contrasenya.setText("");
         }
+        
+        
+        
     }
     
-}
+    
+}    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
